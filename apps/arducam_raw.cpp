c7d79250bc5663bdd33656bdefea6af208d0afd7
Armolina <armolina@iuma.ulpgc.es>
2024-04-24 10:02:06 +0100

    Logging works and options get executed. Entering event loop

--- apps/arducam_raw.cpp
+++ apps/arducam_raw.cpp
@@ -5,7 +5,7 @@
  * arducam_raw.cpp - IMX477 stereo system raw video record app.
  */
 #include <chrono>
-#inculde <iostream>
+#include <iostream>
 
 #include "core/rpicam_encoder.hpp"
 #include "encoder/arducam_encoder.hpp"
@@ -73,7 +73,7 @@ static void event_loop(ArducamRaw &app)
 
 int main(int argc, char *argv[])
 {
-	srd::cerr << "Before try\n";
+	std::cerr << "Before try\n";
 	try
 	{
 		LOG(1, "Before creating the class");
