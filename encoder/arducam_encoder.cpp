ec2c8f8be33599dc110213f7ef83ea77efc018e4
armolina <armolina@iuma.ulpgc.es>
2024-05-28 17:59:59 +0100

    Add another cout to debug

--- encoder/arducam_encoder.cpp
+++ encoder/arducam_encoder.cpp
@@ -58,7 +58,7 @@ void ArducamEncoder::encodeArducam(ArducamEncoder::EncodeItem &item, uint16_t *&
 {
 	const auto band_width = current_res_->imageWidth/2;
 	const auto band_height = current_res_->imageHeight/2;
-
+	std::cout << "Segmentation fault before casting." << std::endl;
 	auto img_ptr = reinterpret_cast<uint16_t*>(item.mem);
 	std::cout << "Segmentation fault before vector." << std::endl;
 	auto input_image = std::vector<uint16_t>(img_ptr, img_ptr + current_res_->fileWidth*current_res_->fileHeight);
